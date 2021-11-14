b2d6ba9c38514d25dbdc541546fb7c50234d5fb5
vazor <vazor222@gmail.com>
2021-11-14 01:53:29 -0700

    character index selection

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Collections;
 
 // Character changer
 public class ColorChanger : MonoBehaviour
 {
 	public List<Material> characters;
+	public Text chrIdxDisplayTxt;
+	[ReadOnly]
 	public int chrIdx;
 
     // Start is called before the first frame update
@@ -28,6 +32,9 @@ public class ColorChanger : MonoBehaviour
 			chrIdx = 0;
 		else if( chrIdx < 0 )
 			chrIdx = characters.Count-1;
+
+		if( chrIdxDisplayTxt != null )
+			chrIdxDisplayTxt.text = (chrIdx+1).ToString("D2");
 
 		Material newMaterial = characters[chrIdx];
         foreach(Renderer renderer in GetComponentsInChildren<Renderer>())

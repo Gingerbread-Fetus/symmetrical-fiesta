bfe11e0c626e81edf92acbd52b9f8af3ec775e46
vazor <vazor222@gmail.com>
2021-11-14 00:13:01 -0700

    more menu and game scene setup

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
     public void ExitGame()
     {
         Application.Quit();
     }
+
+	public void GoToScene(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
 }

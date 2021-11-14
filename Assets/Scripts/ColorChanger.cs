4d71fed2fb0ace67414b972b980fe88b4ac7c410
vazor <vazor222@gmail.com>
2021-11-13 21:22:53 -0700

    updates to main menu redesign

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Character changer
 public class ColorChanger : MonoBehaviour
 {
+	public List<Material> characters;
+	public int chrIdx;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		chrIdx = Random.Range(0, characters.Count);
     }
 
     // Update is called once per frame
@@ -16,8 +20,16 @@ public class ColorChanger : MonoBehaviour
         
     }
 
-    public void SetMaterial(Material newMaterial)
+    public void SelectNextCharacter(int nextHigher)
     {
+		chrIdx += nextHigher;
+
+		if( chrIdx >= characters.Count )
+			chrIdx = 0;
+		else if( chrIdx < 0 )
+			chrIdx = characters.Count-1;
+
+		Material newMaterial = characters[chrIdx];
         foreach(Renderer renderer in GetComponentsInChildren<Renderer>())
         {
             renderer.material = newMaterial;
